1e9e025a2fbaeec390420151fe0ea42ad51c2346
sn0422j <yotsu.sn.aa@outlook.jp>
2021-09-14 16:46:38 +0000

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-utils",
+    version="0.1.0",
+    description="Python Utilities",
+    author="sn0422j",
+    packages=find_packages("python_utils"),
+    package_dir={"": "python_utils"},
+    python_requires=">=3.7",
+)
